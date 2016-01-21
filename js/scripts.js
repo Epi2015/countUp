3e6ae620b2c96bf521dbac841e868ba3cbb55080
Sean Burgoyne and veronica alley <student@epicodus.com>
2016-01-21 10:23:37 -0800

    refactor js code

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,6 @@
 divCount = function(countTo, countBy) {
   if(countBy <= countTo) {
-    // return true;
-    if (countTo % countBy === 0) {
-      var quotient = countTo / countBy
-      // return quotient;
+      var quotient = Math.floor(countTo / countBy);
       var result = 0;
       var countArray = [];
       for (i = 0; i < quotient; i++) {
@@ -11,20 +8,6 @@ divCount = function(countTo, countBy) {
         countArray.push(result);
       }
         return countArray;
-
-    }else{
-        var quotient = (countTo / countBy);
-        var quotient = Math.floor(quotient);
-        // return quotient;
-        var result = 0;
-        var countArray = [];
-        for (i = 0; i < quotient; i++) {
-          var result = result + countBy;
-          countArray.push(result);
-        }
-          return countArray;
-    }
-
   }else {
     return false;
     alert("Make sure your divisor is less than your dividend");
